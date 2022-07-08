7073df68b41648fe44302a2159d506399c9d9dce
LODS <91239977+LODSX@users.noreply.github.com>
2022-07-07 21:49:18 -0300

    Update slide bar
    New features added:
    - smooth scroll;
    - function to set property;
    - small optimizations

--- slidebar.lua
+++ slidebar.lua
@@ -1,11 +1,3 @@
---[[credits:
-
-    author: LODS,
-    description: "A simple slidebar",
-    version: "1.0"
-
-]]--
-
 -- small optimizations
 local tblInsert = table.insert
 local tblRemove = table.remove
@@ -15,33 +7,36 @@ local ceil = math.ceil
 local tblMax = table.maxn
 local max = math.max
 local min = math.min
+local interpolate = interpolateBetween
 
 local screenW, screenH = guiGetScreenSize()
 
-local inputHover
 local scrollInput = {}
+local inputHover
+local posI, posF
+local typeProgress, tickSmooth
 
 scrollInput.inputs = {}
 
 -- create the scroll input
 function createScrollInput(x, y, width, height, radiusBorder, minValue, maxValue, circleScale, postGUI)
     if (not (x or y)) then
-        local input = (not x and "Erro no argumento #1. Defina uma posição X") or (not y and "Erro no argumento #2. Defina uma posição Y")
-        error(input, 2)
+        local input = (not x and "Error in argument #1. Define a position X") or (not y and "Error in argument #2. Define a position Y")
+        warn(input)
     end
     if (not (width or height)) then
-        local input = (not width and "Erro no argumento #3. Defina uma largura") or (not height and "Erro no argumento #4. Defina uma altura")
-        error(input, 2)
+        local input = (not width and "Error in argument #3. Define a width") or (not height and "Error in argument #4. Define a height")
+        warn(input)
     end
     if ((width == 0) or (height == 0)) then
-        local input = ((width == 0) and "Erro no argumento #3. Defina uma largura maior que 0") or ((height == 0) and "Erro no argumento #4. Defina uma altura maior que 0")
-        error(input, 2)
+        local input = ((width == 0) and "Error in argument #3. Define a width greater than 0") or ((height == 0) and "Error in argument #4. Define a height greater than 0")
+        warn(input)
     end
     if (minValue > maxValue) then
-        error("Erro no argumento #5. O valor mínimo não pode ser maior que o valor máximo.", 2)
+        warn("Error in argument #5. The min value don't can't be greater than max value")
     end
     if (maxValue < minValue) then
-        error("Erro no argumento #6. O valor máximo não pode ser menor que o valor mínimo.", 2)
+        warn("Error in argument #6. The max value don't can't be smaller than min value")
     end
 
     radius = radiusBorder and height/2 or 0
@@ -52,13 +47,13 @@ function createScrollInput(x, y, width, height, radiusBorder, minValue, maxValue
 
     local rawDataCircle = strFormat([[
         <svg width="%s" height="%s" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="%s" cy="%s" r="%s" fill="#FFFFFF" />
+            <circle cx="%s" cy="%s" r="%s" fill="#FFFFFF"/>
         </svg>
     ]], circleScale, circleScale, newX, newY, circleRadius)
 
     local rawDataBar = strFormat([[
         <svg width="%s" height="%s" xmlns="http://www.w3.org/2000/svg">
-            <rect rx="%s" width="%s" height="%s" fill="#FFFFFF" />
+            <rect rx="%s" width="%s" height="%s" fill="#FFFFFF"/>
         </svg>
     ]], width, height, radius, width, height)
 
@@ -73,10 +68,10 @@ function createScrollInput(x, y, width, height, radiusBorder, minValue, maxValue
         intervalValue = maxValue - minValue,
         radius = radius,
         circleScale = circleScale,
-        selectedBarColor = tocolor(255, 255, 255, 255),
-        selectedBarColorHover = tocolor(255, 255, 255),
-        unselectedBarColor = tocolor(255, 255, 255, 255),
-        circleColor = tocolor(255, 255, 255, 255),
+        barColor = {255, 255, 255},
+        barColorHover = {255, 255},
+        bgBarColor = {255, 255, 255},
+        circleColor = {255, 255, 255},
         postGUI = postGUI or false,
         circle = svgCreate(circleScale, circleScale, rawDataCircle),
         barInput = svgCreate(width, height, rawDataBar),
@@ -88,17 +83,15 @@ function createScrollInput(x, y, width, height, radiusBorder, minValue, maxValue
         -- Technicals
         endedScrolling = true
     }
-    
     datas.circlePos = (datas.x + datas.width)
 
-    dxSetTextureEdge(datas.circle, "mirror")
-    dxSetTextureEdge(datas.barInput, "mirror")
     setmetatable(datas, {__index = scrollInput})
     tblInsert(scrollInput.inputs, datas)
 
     if (tblMax(scrollInput.inputs) == 1) then
         addEventHandler('onClientRender', root, renderScrollInput, false, 'low-5')
         addEventHandler('onClientClick', root, clickScrollInput, false, 'low-5')
+        addEventHandler('onClientKey', root, keyScrollInput, false, 'low-5')
     end
     return datas
 end
@@ -106,11 +99,11 @@ end
 -- create vector images for the scroll input
 local function dxDrawSVG(svg, x, y, width, height, color, postGUI)
     if (not svg) then
-        error('Erro no argumento #1. Defina um svg.', 2)
+        warn("Error in argument #1. Define a SVG.")
     end
     if (not (width or height)) then
-        local input = (not width and 'Erro no argumento #2. Defina uma largura') or (not height and 'Erro no argumento #3. Defina uma altura')
-        error(input, 2)
+        local input = (not width and 'Error in argument #2. Define a width') or (not height and 'Error in argument #3. Define a height')
+        warn(input)
     end
 
     dxSetBlendMode('add')
@@ -128,12 +121,27 @@ function renderScrollInput()
 
     for _, self in ipairs(scrollInput.inputs) do
         local circleY = self.y + ((self.height / 2) - (self.circleScale / 2))
-        local barSelectedColor = self.selectedBarColor
+        local barSelectedColor = self.barColor
         local value = ((self.circlePos - self.x) / self.width) * 100
         local barValue = self.width/100 * value
 
+        if (typeProgress == 'in_progress') then
+            local progress = (getTickCount()-tickSmooth)/250
+            local newCirclePos = interpolate(posI, 0, 0, posF, 0, 0, progress, 'Linear')
+
+            if (progress >= 1) then
+                typeProgress = nil
+                tickSmooth = nil
+                posI = nil
+                posF = nil
+            end
+
+            self.circlePos = newCirclePos
+        end
+
         if (isCursorOnElement(self.x, self.y, ((barValue + self.circleScale) * self.width) / 100, self.height) or isCursorOnElement(self.circlePos - (self.circleScale/2), circleY, self.circleScale, self.circleScale)) then
-            barSelectedColor = self.selectedBarColorHover
+            barSelectedColor = self.barColorHover
+            inputHover = self
         end
 
         if (isCursorOnElement(self.x, self.y, self.width, self.height)) then
@@ -144,26 +152,20 @@ function renderScrollInput()
             local mx, _ = getCursorPosition()
             local cursorX = mx * screenW
 
-            self.circlePos = max(self.x, min((self.x + self.width), cursorX))
-            self.scrollOffset = ceil(max(self.minValue, min(self.maxValue, ((self.circlePos - self.x) / self.width) * 100)))
-            
-            if (self.scrollOffset < 0) then
-                self.scrollOffset = 0
-            end
-            if (self.scrollOffset > 100) then
-                self.scrollOffset = 100
-            end
+            self.circlePos = clamp(cursorX, self.x, (self.x + self.width))
+            self.scrollOffset = floor(clamp(((self.circlePos - self.x) / self.width) * 100, self.minValue, self.maxValue))
+            self.scrollOffset = clamp(self.scrollOffset, 0, 100)
 
             if (self.scroll_event) then
                 self.scroll_event(self.scrollOffset)
             end
 
-            barSelectedColor = self.selectedBarColorHover
+            barSelectedColor = self.barColorHover
         end
 
-        dxDrawSVG(self.barInput, self.x, self.y, self.width, self.height, self.unselectedBarColor, self.postGUI) -- unselectedBar
-        dxDrawSVG(self.barInput, self.x, self.y, barValue, self.height, barSelectedColor, self.postGUI) -- selectedBar
-        dxDrawSVG(self.circle, self.circlePos - (self.circleScale/2), circleY, self.circleScale, self.circleScale, self.circleColor, self.postGUI) -- circle
+        dxDrawSVG(self.barInput, self.x, self.y, self.width, self.height, tocolor(unpack(self.bgBarColor)), self.postGUI) -- unselectedBar
+        dxDrawSVG(self.barInput, self.x, self.y, barValue, self.height, tocolor(unpack(barSelectedColor)), self.postGUI) -- selectedBar
+        dxDrawSVG(self.circle, self.circlePos - (self.circleScale/2), circleY, self.circleScale, self.circleScale, tocolor(unpack(self.circleColor)), self.postGUI) -- circle
 
         if (getKeyState('mouse1')) then
             if (isCursorOnElement(self.circlePos - (self.circleScale/2), circleY, self.circleScale, self.circleScale)) then
@@ -182,28 +184,33 @@ function renderScrollInput()
                 end
             end
         end
+
+        dxDrawText(inspect(self.scrollOffset), 300, 300)
     end
 end
 
 -- function to check if the cursor is on the element and change the position of circle
-function clickScrollInput(b, s)
+function clickScrollInput(button, state)
     if (not scrollInput.inputs or (not (#scrollInput.inputs > 0))) then
         return
     end
 
-    if (b == 'left' and s == 'down') then
-        if (inputHover) then
+    if (inputHover) then
+        if (button == 'left' and state == 'down') then
             local mx, _ = getCursorPosition()
             local cursorX = mx * screenW
 
-            inputHover.circlePos = max(inputHover.x, min((inputHover.x + inputHover.width), cursorX))
-            inputHover.scrollOffset = floor(((inputHover.circlePos - inputHover.x) / inputHover.width) * 100)
 
-            if (inputHover.scrollOffset < 0) then
-                inputHover.scrollOffset = 0
-            end
-            if (inputHover.scrollOffset > 100) then
-                inputHover.scrollOffset = 100
+            if (inputHover.smoothScroll) then
+                inputHover.scrollOffset = floor(((inputHover.circlePos - inputHover.x) / inputHover.width) * 100, 0, 100)
+                local newCirclePos = clamp(cursorX, inputHover.x, (inputHover.x + inputHover.width))
+
+                typeProgress = 'in_progress'
+                tickSmooth = getTickCount()
+                posI, posF = inputHover.circlePos, newCirclePos
+            else
+                inputHover.circlePos = clamp(cursorX, inputHover.x, (inputHover.x + inputHover.width))
+                inputHover.scrollOffset = floor(((inputHover.circlePos - inputHover.x) / inputHover.width) * 100, 0, 100)
             end
 
             if (inputHover.scroll_event) then
@@ -213,10 +220,29 @@ function clickScrollInput(b, s)
     end
 end
 
+-- function to change the offset of slidebar on scroll
+function keyScrollInput(button, press)
+    if (not scrollInput.inputs or (not (#scrollInput.inputs > 0))) then
+        return
+    end
+
+    if (not press) then return end
+
+    if (inputHover) then
+        if (button == 'mouse_wheel_up' and inputHover.scrollOffset < inputHover.maxValue) then
+            inputHover.scrollOffset = inputHover.scrollOffset + 1
+            inputHover:setScrollOffset(inputHover.scrollOffset)
+        elseif (button == 'mouse_wheel_down' and inputHover.scrollOffset > inputHover.minValue) then
+            inputHover.scrollOffset = inputHover.scrollOffset - 1
+            inputHover:setScrollOffset(inputHover.scrollOffset)
+        end
+    end
+end
+
 -- function to destroy the scroll input
 function scrollInput:destroy()
     if (not self) then
-        error("Erro no argumento #1. Defina um objeto.", 2)
+        warn("Error in argument #1. Define a object.")
     end
 
     for i, v in ipairs(scrollInput.inputs) do
@@ -235,17 +261,18 @@ function scrollInput:destroy()
     if (not (tblMax(scrollInput.inputs) > 0)) then
         removeEventHandler('onClientRender', root, renderScrollInput)
         removeEventHandler('onClientClick', root, clickScrollInput)
+        removeEventHandler('onClientKey', root, keyScrollInput)
     end
 end
 
--- function to set scrolloffset
+-- change the width of bar using the value of scrolloffset
 function scrollInput:setScrollOffset(value)
     if (not self) then
-        error("Erro no argumento #1. Defina um objeto.", 2)
+        warn("Error in argument #1. Define a object.")
     end
 
     if (not value) then
-        error("Erro no argumento #2. Defina um valor.", 2)
+        warn("Error in argument #2. Defina a value.")
     end
 
     if (value < 0) then
@@ -255,8 +282,17 @@ function scrollInput:setScrollOffset(value)
         value = 100
     end
 
-    self.scrollOffset = value
-    self.circlePos = self.x + ((self.width / 100) * self.scrollOffset)
+    if (self.smoothScroll) then
+        self.scrollOffset = value
+        local newCirclePos = self.x + ((self.width / 100) * self.scrollOffset)
+        
+        typeProgress = 'in_progress'
+        tickSmooth = getTickCount()
+        posI, posF = self.circlePos, newCirclePos
+    else
+        self.scrollOffset = value
+        self.circlePos = self.x + ((self.width / 100) * self.scrollOffset)
+    end
 end
 
 -- function to get the scroll input
@@ -269,6 +305,63 @@ function scrollInput:onScrollEnd(func)
     self.scrollEnd_event = func
 end
 
+local changeableProperties = {
+    ['x'] = true,
+    ['y'] = true,
+    ['width'] = true,
+    ['height'] = true,
+    ['minValue'] = true,
+    ['maxValue'] = true,
+    ['scrollOffset'] = true,
+    ['smoothScroll'] = true,
+    ['bgBarColor'] = true,
+    ['barColor'] = true,
+    ['barColorHover'] = true,
+    ['circleColor'] = true,
+    -- ['strokeCircle'] = true,
+    -- ['strokeCircleColor'] = true,
+    ['postGUI'] = true,
+}
+
+-- function to change the property of scrollbar
+---comment
+---@param property string
+---@param value any
+---@return boolean
+function scrollInput:setProperty(property, value)
+    if (not self) then
+        warn("No elements were found.") 
+    end
+
+    if (not property or (type(property) ~= 'string')) then
+        local input = (not property and "Error in argument #1. Define a property") or (type(property) ~= 'string' and "Error in argument #1. Define a valid property")
+        warn(input)
+    end
+
+    for propertyIndex, active in pairs(changeableProperties) do
+        if (propertyIndex == property) then
+            self[propertyIndex] = value
+            break
+        end
+    end
+end
+
+-- get the max and min of a value
+function clamp(number, min, max)
+	if (number < min) then
+		return min
+	elseif (number > max) then
+		return max 
+	end
+	return number
+end
+
+-- function to send error in debugscript
+function warn(message)
+    return error(tostring(message), 2)
+end
+
+-- function to get the mouse position absolute
 function isCursorOnElement(x, y, w, h)
     if (not isCursorShowing()) then return end
     local cursor = {getCursorPosition ()}
